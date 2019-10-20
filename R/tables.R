f57480e28c1672abc94502c677d19cbef311b841
Petr Bouchal <pbouchal@gmail.com>
2019-10-21 00:07:40 +0200

    fix dependencies

--- R/tables.R
+++ R/tables.R
@@ -1,3 +1,1 @@
-
-
-available_tables <- tidyr::tribble(~dataset_id, ~table_num, ~table_id, ~table_name, ~dataset_name, ~implemented)
+sp_tables <- tibble::tribble(~dataset_id, ~table_num, ~table_id, ~table_name, ~dataset_name, ~implemented)
