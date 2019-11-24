37622fba947f2fc5a77e20fcc0d6b97bd7410dc8
Petr Bouchal <pbouchal@gmail.com>
2019-11-24 19:28:15 +0100

    Update test-codelists.R

--- tests/testthat/test-codelists.R
+++ tests/testthat/test-codelists.R
@@ -2,8 +2,8 @@ test_that("List of codelists make sense", {
   expect_equivalent(unique(sp_codelists), sp_codelists)
   expect_equivalent(unique(sp_codelists$id), sp_codelists$id)
   expect_equivalent(unique(sp_codelists$name), sp_codelists$name)
-  expect_equivalent(unique(sp_codelists$name), sp_codelists$name)
   expect_equivalent(sort(sp_codelists$id), sp_codelists$id)
+  expect_equivalent(dplyr::distinct(sp_codelists), sp_codelists)
 })
 
 test_that("get_codelist works", {
