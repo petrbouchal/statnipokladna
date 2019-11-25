fa9b2ce44bb4c6f45ee49f54332131b8f699f838
Petr Bouchal <pbouchal@gmail.com>
2019-11-25 21:53:46 +0100

    add utils

---
+++ R/utils.R
@@ -0,0 +1,6 @@
+
+openlinks <- function() {
+  browseURL("http://monitor.statnipokladna.cz/2019/zdrojova-data/transakcni-data")
+  browseURL("http://monitor.statnipokladna.cz/2019/zdrojova-data/ciselniky")
+  browseURL("http://monitor.statnipokladna.cz/")
+}
