3018d036ec72d4fb1a1ae22846be5aacf5ccd3f9
Petr Bouchal <pbouchal@gmail.com>
2020-03-31 16:16:20 +0200

    add release questions

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,10 @@ openlinks <- function() {
   utils::browseURL("http://monitor.statnipokladna.cz/2019/zdrojova-data/ciselniky")
   utils::browseURL("http://monitor.statnipokladna.cz/")
 }
+
+release_questions <- function() {
+  c(
+    "Have you run all examples?",
+    "Have you re-rendered README.Rmd?"
+  )
+}
