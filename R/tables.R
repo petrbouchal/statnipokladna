7a9c82473002b1d846d1ec8a2e269ab7e460abdb
Petr Bouchal <pbouchal@gmail.com>
2020-12-28 00:54:22 +0100

    nicer missing param warning

--- R/tables.R
+++ R/tables.R
@@ -255,11 +255,18 @@ sp_get_table <- function(table_id, year = 2018, month = 12, ico = NULL,
                       redownload = FALSE, dest_dir = NULL) {
   stopifnot(is.character(ico) | is.null(ico))
   if(interactive() == FALSE & (missing(year) | missing(month))) {
-    ui_warn("Either {ui_field('year')} or {ui_field('month')} not set.
-                     Using defaults of {ui_value(year)} and {ui_value(month)}.")
-    ui_todo("Set these values explicitly for reproducibility as the defaults may change in the future
-                     to provide access to the latest data by default.")
+    if(missing(year)) {
+      ui_warn("{ui_field('year')} not set.
+                     Using default of {ui_value(year)}.")
 
+    } else if(missing(month)) {
+      ui_warn("{ui_field('month')} not set.
+                     Using default of {ui_value(month)}.")
+
+    }
+
+    ui_todo("Set period parameters explicitly for reproducibility as the defaults may change in the future
+                     to provide access to the latest data by default.")
   }
 
   if(is.null(dest_dir)) dest_dir <- getOption("statnipokladna.dest_dir",
