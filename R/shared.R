a675d1e4f990a0f172be16a2fe9d34459ffd9c0b
Petr Bouchal <pbouchal@gmail.com>
2021-05-16 22:53:37 +0200

    more flexible codelist viewer URL

--- R/shared.R
+++ R/shared.R
@@ -2,5 +2,5 @@ sp_base_url <- "http://monitor.statnipokladna.cz"
 usr <- "github.com/petrbouchal/statnipokladna"
 
 sparql_url <- "https://opendata.mfcr.cz/lod/sparql"
-codelist_viewer_base <- "https://monitor.statnipokladna.cz/datovy-katalog/ciselniky/prohlizec/"
+codelist_viewer_base <- stringr::str_glue(sp_base_url, "/datovy-katalog/ciselniky/prohlizec/")
 
